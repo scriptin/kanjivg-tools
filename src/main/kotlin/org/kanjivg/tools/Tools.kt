0a163881bda496249373d54976027702fee6263e
Dmitry Shpika <scrptn@gmail.com>
2016-06-11 21:19:00 +0300

    Refactoring

--- src/main/kotlin/org/kanjivg/tools/Tools.kt
+++ src/main/kotlin/org/kanjivg/tools/Tools.kt
@@ -11,35 +11,21 @@ import javax.xml.stream.XMLInputFactory
 class Tools(final val config: Config) {
     private final val logger = LoggerFactory.getLogger(javaClass)
 
+    private final val validations = listOf(
+        WidthAndHeight,
+        ViewBox,
+        StrokeRootGroupId,
+        StrokeRootGroupStyle,
+        NumberRootGroupId,
+        NumberRootGroupStyle
+    )
+
     fun validate(): Unit {
-        val dir = config.getString("kanjivgDir")
-        logger.info("Scanning KanjiVG directory: {}", dir)
-        val files: List<File> = File(dir).walk().onFail { file, ioException ->
-            throw RuntimeException("Error processing a file '$file'", ioException)
-        }.filter { it.isFile }.toList()
-        logger.info("Found {} files", files.size)
-
-        val xmlInputFactory = XMLInputFactory.newInstance()
-        // 'kvg:' prefix causes error if namespace awareness is on
-        xmlInputFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false)
-        xmlInputFactory.setProperty(XMLInputFactory.IS_VALIDATING, false)
-        // Fetching DTD makes the parsing extremely slow
-        xmlInputFactory.setXMLResolver { publicID, systemID, baseURI, namespace -> "".byteInputStream() }
-
-        val validations = listOf(
-            WidthAndHeight,
-            ViewBox,
-            StrokeRootGroupId,
-            StrokeRootGroupStyle,
-            NumberRootGroupId,
-            NumberRootGroupStyle
-        )
-
-        val validationDescriptions = validations.map { "${it.name}: ${it.description}" }
-        logger.info("Validations:\n  ${validationDescriptions.joinToString("\n  ")}")
-
+        val files = getFiles(config.getString("kanjivgDir"))
+        val xmlInputFactory = getXMLInputFactory()
+        printValidationsInfo(validations)
         files.forEach { file ->
-            logger.info("START: {}", file.name)
+            logger.info("===== {} =====", file.name)
             val kanjiCode = file.nameWithoutExtension
             val eventReader = xmlInputFactory.createXMLEventReader(FileReader(file))
             try {
@@ -56,7 +42,7 @@ class Tools(final val config: Config) {
                     )
                 }
             } catch (e: ParsingException) {
-                logger.error("PARSING FAILED: {}\n{}", file.name, e.message)
+                logger.error("PARSING FAILED: {} - {}", file.name, e.message)
                 logger.debug("Stack trace:", e)
             } catch (e: Throwable) {
                 logger.error("UNHANDLED ERROR: {}", file.name, e)
@@ -64,5 +50,29 @@ class Tools(final val config: Config) {
                 eventReader.close()
             }
         }
+    }
+
+    private fun getFiles(dir: String): List<File> {
+        logger.info("Scanning KanjiVG directory: {}", dir)
+        val files: List<File> = File(dir).walk().onFail { file, ioException ->
+            throw RuntimeException("Error processing a file '$file'", ioException)
+        }.filter { it.isFile }.toList()
+        logger.info("Found {} files", files.size)
+        return files
+    }
+
+    private fun getXMLInputFactory(): XMLInputFactory {
+        val xmlInputFactory = XMLInputFactory.newInstance()
+        // 'kvg:' prefix causes error if namespace awareness is on
+        xmlInputFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false)
+        xmlInputFactory.setProperty(XMLInputFactory.IS_VALIDATING, false)
+        // Fetching DTD makes the parsing extremely slow
+        xmlInputFactory.setXMLResolver { publicID, systemID, baseURI, namespace -> "".byteInputStream() }
+        return xmlInputFactory
+    }
+
+    private fun printValidationsInfo(validations: List<Validation>) {
+        val validationDescriptions = validations.map { "${it.name}: ${it.description}" }
+        logger.info("Validations:\n  ${validationDescriptions.joinToString("\n  ")}")
     }
 }
