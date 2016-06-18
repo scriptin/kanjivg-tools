6a053628b10a0aaebc8091aa02096f117e92c39a
Dmitry Shpika <scrptn@gmail.com>
2016-06-18 23:03:29 +0300

    Refactoring: Validation.getStrokes(group) method to get a flat collection of strokes

--- src/main/kotlin/org/kanjivg/tools/validation/StrokeIds.kt
+++ src/main/kotlin/org/kanjivg/tools/validation/StrokeIds.kt
@@ -8,15 +8,5 @@ object StrokeIds : IdsValidation(
 ) {
     override fun suffix(idx: Int): String = "-s${idx + 1}"
 
-    override fun getIds(group: KVGTag.StrokePathsSubGroup): List<String> {
-        val ids = mutableListOf<String>()
-        group.children.forEach { child ->
-            when (child) {
-                is KVGTag.StrokePathsSubGroup -> ids.addAll(getIds(child))
-                is KVGTag.Path -> ids.add(child.id.value)
-                else -> throw IllegalStateException("Found a child of type ${child.javaClass.name} inside stroke group")
-            }
-        }
-        return ids
-    }
+    override fun getIds(group: KVGTag.StrokePathsSubGroup): List<String> = getStrokes(group).map { it.id.value }
 }
--- src/main/kotlin/org/kanjivg/tools/validation/Validation.kt
+++ src/main/kotlin/org/kanjivg/tools/validation/Validation.kt
@@ -7,4 +7,19 @@ import org.kanjivg.tools.KVGTag
  */
 abstract class Validation(open val name: String, open val description: String) {
     abstract fun validate(fileId: String, svg: KVGTag.SVG): ValidationResult
+
+    /**
+     * Get a flat list of strokes from a given stroke group
+     */
+    fun getStrokes(group: KVGTag.StrokePathsSubGroup): List<KVGTag.Path> {
+        val strokes = mutableListOf<KVGTag.Path>()
+        group.children.forEach { child ->
+            when (child) {
+                is KVGTag.StrokePathsSubGroup -> strokes.addAll(getStrokes(child))
+                is KVGTag.Path -> strokes.add(child)
+                else -> throw IllegalStateException("Found a child of type ${child.javaClass.name} inside stroke group")
+            }
+        }
+        return strokes
+    }
 }
